06f460803d0d6183ac6e3cad987de9d1305b355e
ank-21 <ankitSrivastava21598@gmail.com>
2020-01-21 13:02:38 +0530

    src file updated

--- src/app.js
+++ src/app.js
@@ -12,15 +12,15 @@ import './styles/styles.css';
 const store = configureStore();
 
 
-store.dispatch(addExpense({description: 'Water Bill', amount:4500, createdAt:0 }));
-store.dispatch(addExpense({description: 'Gas Bill', amount:0, createdAt:1000 }));
-store.dispatch(addExpense({description: 'Rent', amount:19500 }));
+// store.dispatch(addExpense({description: 'Water Bill', amount:4500, createdAt:0 }));
+// store.dispatch(addExpense({description: 'Gas Bill', amount:0, createdAt:1000 }));
+// store.dispatch(addExpense({description: 'Rent', amount:19500 }));
 
 // store.dispatch(setTextFilter('water'));
 
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses,state.filters);
-console.log(visibleExpenses);
+// const state = store.getState();
+// const visibleExpenses = getVisibleExpenses(state.expenses,state.filters);
+// console.log(visibleExpenses);
 
 const jsx = (
     <Provider store= {store}>
